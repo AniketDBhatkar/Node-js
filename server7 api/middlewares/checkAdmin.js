654bb67c4bb6bcd26c9765a5d79cb2832f55d7fa
AniketDBhatkar <aniketbhatkar786@gmail.com>
2025-10-03 18:14:55 +0530

    user register

---
+++ server7 api/middlewares/checkAdmin.js
@@ -0,0 +1,17 @@
+let checkAdmin = async (req, res, next) => {
+    try {
+
+        // decoded token
+
+        console.log(req.headers)
+        let { admin } = req.headers
+        if (!admin) throw ("not a valid admin !")
+        next()
+    } catch (err) {
+        console.log("error from checkAdmin middleware : ", err)
+        res.status(401).json({ message: "failed to execute process !", err })
+    }
+}
+
+export { checkAdmin }
+
--- server7 api/server.js
+++ server7 api/server.js
@@ -1,12 +1,15 @@
 import express from "express"
 import dotenv from "dotenv"
+import "./database/conndb.js"
 import { router } from "./routers/router.js"
+import mongoose from "mongoose"
+import { userRouter } from "./routers/userRouter.js"
 
 dotenv.config({ path: "./config.env" })
 
 const app = express()
 
-let port = process.env.PORT || 5005
+let port = process.env.PORT || 3007
 
 app.use(express.urlencoded({ extended: true }))
 
@@ -14,7 +17,11 @@ app.use(express.json())
 
 app.use(express.static("public"))
 
+app.get('/', (req, res) => { res.redirect("/languages/api/get-details") })
+
 app.use("/languages/api", router)
+
+app.use("/user/api", userRouter)
 
 app.use((req, res) => {
     console.log("someone is trying to access a 404 route !")
